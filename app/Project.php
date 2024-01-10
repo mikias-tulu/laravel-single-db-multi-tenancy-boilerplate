da108f6c456eff18e5319a8cbbff2506aa55318a
mikias-tulu <miki.tulu@gmail.com>
2024-01-10 03:00:00 +0300

    feat(tenant): create project model to support multi-tenancy

---
+++ app/Project.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use App\Tenant\Traits\ForTenants;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use ForTenants;
+
+    protected $fillable = [
+        'name'
+    ];
+}
